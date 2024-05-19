b278e9c0e25dbfda27f0ef0bb1eb6bedb45ff49a
YasinCodeCo <m.yasin.abspor@gmail.com>
2024-05-19 13:06:47 +0330

    import project and creat routing and context and config axios

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useEffect, useState } from "react";
+import api from "./../services/config";
+const ProductContext = createContext();
+
+function ProductProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+  }, []);
+
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
+
+export default ProductProvider;
